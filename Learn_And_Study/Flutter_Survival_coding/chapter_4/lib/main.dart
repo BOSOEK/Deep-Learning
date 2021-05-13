0eaf2a7c9a30a6d598ac09ffadb47a7596b0b86c
BOSOEK <thydcj@naver.com>
2021-05-13 16:57:16 +0900

    챕터 4 완료!

--- Learn_And_Study/Flutter_Survival_coding/chapter_4/lib/main.dart
+++ Learn_And_Study/Flutter_Survival_coding/chapter_4/lib/main.dart
@@ -21,34 +21,15 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Container'),
+      appBar: AppBar(
+        title: Text('Container'),
+      ),
+      body: Center(
+        child: FlatButton(
+          child: Text('FlatButton'),
+          onPressed: () {},
         ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0),
-            ),
-            Container(
-              color: Colors.green,
-              width: 100,
-              height: 100,
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0),
-            ),
-            Container(
-              color: Colors.blue,
-              width: 100,
-              height: 100,
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0),
-            ),
-          ],
-        ));
+      ),
+    );
   }
 }
